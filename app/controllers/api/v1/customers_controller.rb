4575a4505b3adb3b008fba254e71064d6757a166
Mohnish G J <mail4mohnish@gmail.com>
2024-08-21 20:46:23 +0200

    Copy customer index action related logic to /api/v1/customers
    - Customer index action code does not need to be nested within Users.

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -7,6 +7,15 @@ module API
 
       before_action :require_all_customer_params, only: %i[create]
 
+      def index
+        customers = Customer.includes(photo_attachment: :blob)
+                            .page(params[:page]).per_page(CUSTOMERS_PER_PAGE)
+
+        return render json: [], status: :ok if customers.blank?
+
+        render json: CustomerSerializer.new(customers).serializable_hash, status: :ok
+      end
+
       def create
         customer = Customer.new(customer_params)
         customer.created_by_id = current_user&.id
