3c4f3ff577b56de0f767398c15ab02dad24bcde8
Mohnish G J <mail4mohnish@gmail.com>
2024-08-30 14:43:31 +0200

    Remove unused code

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -11,8 +11,6 @@ class APIController < ApplicationController
   respond_to :json
 
   def current_user
-    return unless doorkeeper_token
-
     @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
   end
 
